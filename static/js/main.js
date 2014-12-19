90980e436656715feb36cf6cd3c9f883839ae98e
Mauricio Ivan Mejia Ramirez <mejiaramirez@outlook.com>
2014-12-18 22:31:19 -0600

    Arregaldo bug del menu lateral al hacer scroll cuando esta activo

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,17 @@
 var $section = document.querySelector('section');
 var $buttonMenu = document.getElementById('button-action');
 var $menu = document.getElementById('menu-slide');
+var $body = document.querySelector('body');
 
 
 var showMenu = function () {
 	$menu.classList.add('is-active');
+	$body.classList.add('hidden');
 }
 
 var hideMenu = function () {
 	$menu.classList.remove('is-active');
+	$body.classList.remove('hidden');
 }
 
 $buttonMenu.addEventListener("mouseover",showMenu);
