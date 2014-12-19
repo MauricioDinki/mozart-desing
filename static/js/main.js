b19ca85f9ebe63fcd6342805deb83f089149fcd6
Mauricio Ivan Mejia Ramirez <mejiaramirez@outlook.com>
2014-12-19 13:16:21 -0600

    Creado menu dropdown para la vista mobil

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,8 @@
-// var $section = document.querySelector('section');
-// var $buttonMenu = document.getElementById('button-action');
-// var $menu = document.getElementById('menu-slide');
-// var $body = document.querySelector('body');
+var $buttonMenu = $('#button-action')
+var $menu = $('#menu-slide')
 
+function mostrarOcultarFormulario(){
+	$menu.slideToggle();
+}
 
-// var showMenu = function () {
-// 	$menu.classList.add('is-active');
-// }
-
-// var hideMenu = function () {
-// 	$menu.classList.remove('is-active');
-// }
-
-// $buttonMenu.addEventListener("click",showMenu);
-// $buttonMenu.addEventListener("click",hideMenu);
-
-// var mc = new Hammer($section);
-
-// mc.on('panleft',showMenu);
-// mc.on('panright',hideMenu);
-
-// mc.add(new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 50 }));
+$buttonMenu.on('click',mostrarOcultarFormulario)
